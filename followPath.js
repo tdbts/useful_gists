f33afe1e677b34eb98cdeddd6e85dee3c8615f91
Vinny Sanchez <VinnySanchez87@gmail.com>
2016-01-25 20:33:56 -0500

    Add followPath.js gist.

---
+++ followPath.js
@@ -0,0 +1,27 @@
+function followPath(path, obj) {
+	var result; 
+
+	if (path.length === 0) {
+		result = obj; 
+	} else {
+		result = followPath(path.slice(1), obj[path[0]])
+	}
+
+	return result; 
+}
+
+/* 
+Example use: 
+
+var myObj = {
+	prop1: {
+		propA: {
+			propOne: "you found me", 
+			propTwo: false
+		}	
+	}, 
+	prop2: "nope"
+};
+
+followPath(['prop1', 'propA', 'propOne'], myObj); // ==> "you found me"
+*/
