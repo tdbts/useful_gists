622aaad55c3cc13e02a43f272dbfe8ce78df49b5
Vinny Sanchez <VinnySanchez87@gmail.com>
2016-02-15 23:04:12 -0500

    Add poll.js gist.

---
+++ poll.js
@@ -0,0 +1,19 @@
+function poll(fn, callback, errback, timeout, interval) {
+    var endTime = Number(new Date()) + (timeout || 2000);
+    interval = interval || 100;
+
+    (function p() {
+            // If the condition is met, we're done! 
+            if(fn()) {
+                callback();
+            }
+            // If the condition isn't met but the timeout hasn't elapsed, go again
+            else if (Number(new Date())  0;
+    },
+    function() {
+        // Done, success callback
+    },
+    function() {
+        // Error, failure callback
+    }
+);
