b4168f1d06b33b9547e46a7eb970a5c34fff0b27
Vinny Sanchez <VinnySanchez87@gmail.com>
2016-03-07 22:35:19 -0500

    Update followPath.js to use ES6 and new options.

--- followPath.js
+++ followPath.js
@@ -1,14 +1,65 @@
-function followPath(path, obj) {
-	var result; 
+import getType from './getType'; 
+import isType from './isType'; 
+
+/**
+ *
+ * @name followPath() 
+ *
+ * @desc Returns the entity located at the terminus of the 
+ *     given path within the given object of nested objects.
+ *
+ * @param { string[] } path - Array of strings delineating 
+ *     the path to follow.  
+ * @param { object } obj - The object where the path will be  
+ *     followed. 
+ * @param { object } options - An object for setting options.
+ *     - @prop { boolean } upsert - If true, in cases where the 
+ *         property does not exist within the current object, an 
+ *         empty object will be created at that property. 
+ *     - @prop { function } terminus - A function that will be 
+ *         called in order to set the value of the property 
+ *         located at the terminus of the given path.     
+ * 
+ * @returns { any }
+ *
+ */
+
+const followPath = (path, obj, options) => {
+	let result;  
+	let prop; 
 
 	if (path.length === 0) {
 		result = obj; 
+	
 	} else {
-		result = followPath(path.slice(1), obj[path[0]])
+		prop = path[0]; 
+
+		if ('object' === getType(obj)) {
+			if (!(prop in obj)) {
+
+				if ((path.length === 1) && ('terminus' in options) && (isType('function', options.terminus))) {
+					obj[prop] = options.terminus(prop); 
+				
+				} else if (('upsert' in options) && options.upsert) {
+					obj[prop] = {}; 
+				
+				} else {
+					throw new ReferenceError(`The property ${prop} does not exist in the object ${JSON.stringify(obj)}`); 
+				}
+			}
+		
+		} else {
+			throw new Error(`The path ${path} is invalid for the entity ${JSON.stringify(obj)}`); 
+		}
+
+		result = followPath(path.slice(1), obj[prop], options); 
 	}
 
 	return result; 
-}
+};
+
+export default followPath; 
+
 
 /* 
 Example use: 
